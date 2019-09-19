9307ea496b2b4a959789cff8476b07591bd9544e
Arthur <kingart808@gmail.com>
2019-09-19 11:33:52 -1000

    got routes done and authorization done

--- database/seeds/gallery.js
+++ database/seeds/gallery.js
@@ -6,8 +6,8 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('gallery').insert([
         {author: 'aliko dangote',link: 'https://specials-images.forbesimg.com/imageserve/5c33a1554bbe6f7020fb2fd2/416x416.jpg?background=000000&cropX1=363&cropX2=3282&cropY1=11&cropY2=2930',description: 'Nigerian, Sugar, Cement, Flour',user_id:'1'},
-        {author: 'mike adenuga',link: 'https://buzznigeria.com/wp-content/uploads/2015/01/Mike-Adenuga-Jnr-Chairman-of-Globacom-640x548.jpg',description:'Nigerian, Oil, Telecoms',user_id:'1'},
-        {author: 'robert smith',link:'https://specials-images.forbesimg.com/imageserve/5ba5310331358e5690cdb03e/416x416.jpg?background=000000&cropX1=737&cropX2=3137&cropY1=268&cropY2=2667',description:'American, Private Equity',user_id:'2'}
+        {author: 'mike adenuga',link: 'https://buzznigeria.com/wp-content/uploads/2015/01/Mike-Adenuga-Jnr-Chairman-of-Globacom-640x548.jpg',description:'Nigerian, Oil, Telecoms',user_id:'2'},
+        {author: 'robert smith',link:'https://specials-images.forbesimg.com/imageserve/5ba5310331358e5690cdb03e/416x416.jpg?background=000000&cropX1=737&cropX2=3137&cropY1=268&cropY2=2667',description:'American, Private Equity',user_id:'3'}
       ]);
     });
 };
--- routes/gallery.js
+++ routes/gallery.js
@@ -3,42 +3,72 @@ const router = express.Router();
 router.get("/", (req, res) => {
   return req.db.Gallery.fetchAll().then(results => {
     let all = results.toJSON();
-    console.log(all);
-    
+
     res.render("./templates/index", { all: all });
   });
 });
-router.get('/login',(req,res)=>{
-  res.render('./login')
-})
 
-router.get('/new',(req,res)=>{
-    res.render('./templates/new')
-})
+router.get("/new", (req, res) => {
+  res.render("./templates/new");
+});
 
-router.get('/:id/edit', (req,res)=>{
-    res.render('./templates/edit')
-})
+router.get("/:id/edit", (req, res) => {
+  return req.db.Gallery.forge({ id: req.params.id })
+    .fetch()
+    .then(results => {
+      let post = results.toJSON();
+      if(req.user.id !== post.user_id){
+        return res.send('You are not allowed to make changes to this post')
+      }     
+       res.render("./templates/edit", post);
+    });
+});
 
 router.get("/:id", (req, res) => {
   return req.db.Gallery.forge({ id: req.params.id })
     .fetch()
     .then(results => {
       let post = results.toJSON();
-      console.log(post);
       res.render("./templates/post", post);
     });
 });
 
-
-
-router.post('/',(req,res)=>{
-    console.log(req.body)
-    return req.db.Gallery.forge({link: req.body.link,
+router.post("/", (req, res) => {
+  return req.db.Gallery.forge({
+    link: req.body.link,
     author: req.body.author,
-description: req.body.description}).save().then(results=>{
-    res.redirect('gallery')
-})
+    description: req.body.description,
+    user_id: req.user.id
+  })
+    .save()
+    .then(results => {
+      res.redirect("/");
+    });
+});
+
+router.put("/:id", (req, res) => {
+  return req.db.Gallery.forge({ id: req.params.id })
+    .save({
+      author: req.body.author,
+      link: req.body.link,
+      description: req.body.description
+    })
+    .then(results => {
+      res.redirect(`/${req.params.id}`);
+    });
+});
+
+router.delete('/:id',(req,res)=>{
+  return req.db.Gallery.forge({id: req.params.id}).fetch().then(results=>{
+    let post = results.toJSON()
+    if(req.user.id !== post.user_id){
+      return res.send('You are not allowed to make changes to this post')
+    }
+    return req.db.Gallery.forge({id: req.params.id}).destroy().then(results =>{
+      
+      res.redirect('/')
+    })    
+  })
 })
 
 module.exports = router;
